5e425d2f7eb637d36a6d1d91a0cd3368b2208511
ayush-ktr <34536098+ayush-ktr@users.noreply.github.com>
2024-05-20 18:58:07 +0530

    removing author name

--- src/main/java/com/reactor/util/User.java
+++ src/main/java/com/reactor/util/User.java
@@ -1,12 +1,5 @@
 package com.reactor.util;
 
-/**
- * This class is a sample custom Java type used in the exercises.
- * DO NOT MODIFY THIS CODE
- *
- * @author koushikkothagal
- */
-
 public class User {
 
     private int id;
